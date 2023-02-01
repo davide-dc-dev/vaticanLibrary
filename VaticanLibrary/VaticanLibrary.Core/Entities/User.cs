89e2d8b67a8dc9d25bbb3774eb91400bcf3ba51e
Umberto Cuomo (External) <e-umberto.cuomo@msc.com>
2023-02-01 14:21:31 +0100

    user Class

--- VaticanLibrary/VaticanLibrary.Core/Entities/User.cs
+++ VaticanLibrary/VaticanLibrary.Core/Entities/User.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace VaticanLibrary.Core.Entities
 {
-    internal class User
+    public class User
     {
+        public string Name;
+        public string Surname;
+        public int ID;
+        public DateOnly DateSubscription;
+        public DateOnly LastLoan;
+
+        User(string name, string surname, int id, DateOnly lastLoan)
+        {
+            Name = name;
+            Surname = surname;
+            ID = id;
+            LastLoan = lastLoan;
+            DateSubscription = DateOnly.FromDateTime(DateTime.Now);
+        }
     }
 }
