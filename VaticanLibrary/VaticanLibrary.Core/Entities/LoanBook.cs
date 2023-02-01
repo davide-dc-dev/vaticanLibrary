38baaa6b10a31c3cd14dbe172086b2adb28fb5f8
Davide De Cenzo (External) <e-davide.decenzo@msc.com>
2023-02-01 14:14:15 +0100

    Aggiunta classe LoanBook

--- VaticanLibrary/VaticanLibrary.Core/Entities/LoanBook.cs
+++ VaticanLibrary/VaticanLibrary.Core/Entities/LoanBook.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace VaticanLibrary.Core.Entities
 {
-    internal class LoanBook
+    public class LoanBook
     {
+
+        public string IdUser { get; set; }
+        public string ISBN { get; set; }
+
+        public DateOnly StartLoan { get; set; }
+
+        public DateOnly EndLoan { get; set; }
+
+        public LoanBook(string idUser, string isbn, DateOnly startLoan, DateOnly endLoan)
+        {
+            IdUser = idUser;
+            ISBN = isbn;
+            StartLoan = startLoan;
+            EndLoan = endLoan;
+        }
     }
 }
