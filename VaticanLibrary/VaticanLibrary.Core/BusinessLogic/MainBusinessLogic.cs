0ab8506c6e9c84be118f8aa361e3ed0ea3aad671
Davide De Cenzo (External) <e-davide.decenzo@msc.com>
2023-02-01 12:58:34 +0100

    Inizialization

---
+++ VaticanLibrary/VaticanLibrary.Core/BusinessLogic/MainBusinessLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaticanLibrary.Core.BusinessLogic
+{
+    internal class MainBusinessLogic
+    {
+    }
+}
